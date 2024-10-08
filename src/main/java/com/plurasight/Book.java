339c10ea9f2a7b3ce3d8916c47479663290e1349
JacobFranger <jacobfranger@gmail.com>
2024-10-08 09:20:27 -0500

    We git all of our files

---
+++ src/main/java/com/plurasight/Book.java
@@ -0,0 +1,45 @@
+package com.plurasight;
+
+import java.util.Scanner;
+
+public class Book {
+    public static void main(String[] args) {
+
+        System.out.print("Check In: \n");
+
+
+
+        Scanner in = new Scanner(System.in);    
+        StringBuilder book = new StringBuilder();
+        book.append("The Bible \n");
+        book.append("Hog Warts \n");
+        book.append("The Twilight Saga \n");
+        book.append("The Lord of the Rings \n");
+        book.append("The Alchemist \n");
+        book.append("Good Material \n");
+        book.append("Victim \n");
+        book.append("Pride and Prejudice \n");
+        book.append("Frankenstein \n");
+        book.append("Greyfriars Bobby \n");
+        book.append("Little Women \n");
+        book.append("Orlando \n");
+        book.append("Narrative of the Life of Frederick Douglass \n");
+        book.append("The Great Gatsby \n");
+        book.append("The Artist's Way \n");
+        book.append("You Owe You \n");
+        book.append("The Sleep Revolution \n");
+        book.append("The Gift of Forgiveness \n");
+        book.append("The Four Agreements \n");
+        book.append("Untamed: Reese's Book Club \n");
+        book.append("The Da Vinci Code \n");
+        String tittle = book.toString();
+        System.out.println(tittle);
+
+        System.out.println("Check Out: ");
+
+
+
+    }
+    }
+
+
